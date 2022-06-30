d3ddd5c5faff0488dbcb4d9f626c8427b55ca6f1
Carlos Rojas Quiroz <carlosrojasquiroz@gmail.com>
2022-06-30 23:42:46 +0200

    Delete Master/OtherTry directory

--- Master/OtherTry/One parameter/solversearch.m
+++
@@ -1,9 +0,0 @@
-function [chiTss0,chiNss0,Jss0]=solversearch(p,v,d,ii)
-%
-fun = @(param) inference(param,p,v,d,ii.W);
-x0 = ii.chiTopt0;
-[chiss0,Jss0] = fminsearch(fun,x0);
-%
-chiTss0=chiss0;
-chiNss0=chiss0;
-end
--- Master/OtherTry/One parameter/utility.m
+++
@@ -1,5 +0,0 @@
-function u=utility(p,c,hT,hN)
-cNeg=(c-(hT.^p.omegaT)/p.omegaT-(hN.^p.omegaN)/p.omegaN<=0);
-u = ((c-(hT.^p.omegaT)/p.omegaT-(hN.^p.omegaN)/p.omegaN).^(1-p.sigma)-1)./ (1-p.sigma);
-u(cNeg)=-Inf;
-end
